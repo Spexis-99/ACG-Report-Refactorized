fda82f02c676eade578ff8ee300c4d2539724a80
Spexis-99 <95927966+Spexis-99@users.noreply.github.com>
2022-12-20 15:18:33 +0200

    a

--- FoldersToCopy/js/jsonbuildtable.js
+++ FoldersToCopy/js/jsonbuildtable.js
@@ -1,27 +1,17 @@
-	function buildTable(table_id,data){
-		var table = document.getElementById(table_id)
-		for (var i = 0; i < data.length; i++){
-			var row = `<tr>`;
-			data[i].foreach(){
-				var row=row+`${data[i].name}`;
-
-			};
-			
-					// 		<td>${data[i].name}</td>
-					// 		<td>${data[i].age}</td>
-					// 		<td>${data[i].birthdate}</td>
-					//   </tr>`
-
-
-			table.innerHTML += row
-
-
-
-			var row = `<tr>`;
-			data.forEach(obj => {
-				Object.entries(obj).forEach(([key, value]) => {
-					console.log(`${key} ${value}`);
-					row=row + `${value}`;
-				});
-		}
-	}
+function buildTable(table_id, filename) {
+	fetch(filename).then(response => {
+		return response.json();
+	}).then(data => {
+		// Work with JSON data here
+		console.log(data);
+		data.forEach(element => {
+			row = "<tr><td></td>"
+			element.forEach(innerelement => {
+				row += "<td>" + innerelement + "</td>";
+			}
+			)
+			row += "</tr>";
+			$("#" + table_id).append(row);
+		});
+	});
+}
