eda69d5757713dbe43f8a6c654549fc9c868bc8f
Spexis-99 <95927966+Spexis-99@users.noreply.github.com>
2022-12-21 14:33:42 +0200

    a

--- FoldersToCopy/js/jsonbuildtable.js
+++ FoldersToCopy/js/jsonbuildtable.js
@@ -1,4 +1,9 @@
-function buildTable(table_id, filename) {
+function 	buildTable(table_id, filename) {
+	/**
+ 		* Fill table from json file using id and filename
+ 		* @param  {String} table_id Selector for the table
+ 		* @param  {String} filename File name and directory
+ 	*/
 	fetch(filename).then(response => {
 		return response.json();
 	}).then(data => {
