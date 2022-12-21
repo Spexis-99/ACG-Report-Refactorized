6934acb776ecbe95c0229e9d7f1f590f3a9d55bd
Spexis-99 <95927966+Spexis-99@users.noreply.github.com>
2022-12-21 16:46:17 +0200

    a

--- FoldersToCopy/js/filter.js
+++ FoldersToCopy/js/filter.js
@@ -1,5 +1,26 @@
-
-
+document.writeln('<script src="/js/jquery-3.4.1.min.js" type="text/javascript"></script>');
+function 	buildTable(table_id, filename) {
+	/**
+ 		* Fill table from json file using id and filename
+ 		* @param  {String} table_id Selector for the table
+ 		* @param  {String} filename File name and directory
+ 	*/
+	fetch(filename).then(response => {
+		return response.json();
+	}).then(data => {
+		// Work with JSON data here
+		console.log(data);
+		data.forEach(element => {
+			row = "<tr><td></td>"
+			element.forEach(innerelement => {
+				row += "<td>" + innerelement + "</td>";
+			}
+			)
+			row += "</tr>";
+			$("#" + table_id).append(row);
+		});
+	});
+}
 function getUniqueValuesFromColumn() {
 
     var unique_col_values_dict = {}
